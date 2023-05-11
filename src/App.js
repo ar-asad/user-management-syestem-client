941990c08007f17818fc0d90d676498a31d5bd2c
arasad1 <asaduzzamanaj07@gmail.com>
2023-05-11 19:11:45 +0600

    header and home implement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
+import router from './Router/Routes/Routes';
 
 function App() {
   return (
---
+++ src/shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='bg-green-500'>
+            <h2 className='text-2xl text-center p-4 font-semibold'>User Management Syestem</h2>
+        </div>
+    );
+};
+
+export default Header;
