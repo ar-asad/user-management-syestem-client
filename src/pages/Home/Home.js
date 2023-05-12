088a1028741eb8727183d0b0f45682fc1775be6f
arasad1 <asaduzzamanaj07@gmail.com>
2023-05-12 12:26:09 +0600

    show all user info in home section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUsers(data))
+    }, [])
+
     return (
-        <div>
-            <h3>this is home component</h3>
+        <div className="overflow-x-auto">
+            <table className="table w-full">
+                {/* head */}
+                <thead >
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Gender</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, i) => <tr key={user._id}>
+                            <th>{i + 1}</th>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.gender}</td>
+                            <td>{user.status}</td>
+                            <td><button className='mr-2 btn btn-xs btn-primary'>update</button><button className='btn btn-xs btn-danger'>Delete</button></td>
+                        </tr>)
+                    }
+
+                </tbody>
+            </table>
         </div>
     );
 };
